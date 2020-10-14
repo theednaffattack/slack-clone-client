87fa160f81f4e63ba888bbbeb90fcea0e7069058
Eddie Naff <eddie@eddienaff.dev>
2020-10-14 16:19:41 -0700

    Add fontSizes to theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,17 +1,29 @@
-import { theme as chakraTheme } from '@chakra-ui/core'
+import { theme as chakraTheme } from "@chakra-ui/core";
 
-const fonts = { ...chakraTheme.fonts, mono: `'Menlo', monospace` }
+const fonts = { ...chakraTheme.fonts, mono: `'Menlo', monospace` };
 
-const breakpoints = ['40em', '52em', '64em']
+const breakpoints = ["40em", "52em", "64em"];
 
 const theme = {
   ...chakraTheme,
   colors: {
     ...chakraTheme.colors,
-    black: '#16161D',
+    black: "#16161D"
   },
   fonts,
-  breakpoints,
-}
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "4rem"
+  },
+  breakpoints
+};
 
-export default theme
+export default theme;
