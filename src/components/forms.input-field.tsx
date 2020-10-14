606b22e678ce048c33580ecad937fed79eb93ed1
Eddie Naff <eddie@eddienaff.dev>
2020-10-14 16:20:12 -0700

    Add generic input field

---
+++ src/components/forms.input-field.tsx
@@ -0,0 +1,34 @@
+import React, { InputHTMLAttributes } from "react";
+import { Field, Form, Formik, useField } from "formik";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input
+} from "@chakra-ui/core";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  isRequired?: boolean;
+  label: string;
+  name: string;
+  placeholder: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  isRequired = false,
+  label,
+  name,
+  placeholder,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField({ name });
+  return (
+    <FormControl isRequired={props.isRequired} isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} placeholder={placeholder} />
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
+};
