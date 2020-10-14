c78f62dc36f981fe47169bac1f178c9054350273
Eddie Naff <eddie@eddienaff.dev>
2020-10-14 16:20:20 -0700

    Add element wrapper

---
+++ src/components/register.wrapper.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/core";
+
+type WrapperProps = {
+  children: React.ReactChildren | React.ReactChild;
+};
+
+export function Wrapper({ children }: WrapperProps) {
+  return (
+    <Box maxW="400px" mx="auto" mt={8} w="100%">
+      {children}
+    </Box>
+  );
+}
