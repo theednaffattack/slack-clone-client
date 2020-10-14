f5390f76fdc1ac24f0f926d183bd56c3e6b43ad1
Eddie Naff <eddie@eddienaff.dev>
2020-10-14 16:20:34 -0700

    Add confirmation page

---
+++ src/pages/confirmation.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Wrapper } from "../components/register.wrapper";
+
+function Confirmation() {
+  return <Wrapper>Confirmation page</Wrapper>;
+}
+
+export default Confirmation;
