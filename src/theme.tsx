b0d31d2502af527e697bc3cc1054f6309791cd29
Eddie Naff <eddie@eddienaff.dev>
2020-10-16 11:54:54 -0700

    Add spaces to theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -2,28 +2,54 @@ import { theme as chakraTheme } from "@chakra-ui/core";
 
 const fonts = { ...chakraTheme.fonts, mono: `'Menlo', monospace` };
 
-const breakpoints = ["40em", "52em", "64em"];
+// const breakpoints = ["40em", "52em", "64em"];
+
+const breakpoints = ["30em", "48em", "62em", "80em"];
+
+const colors = {
+  ...chakraTheme.colors,
+  black: "#16161D"
+};
+
+const fontSizes = {
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "3xl": "1.875rem",
+  "4xl": "2.25rem",
+  "5xl": "3rem",
+  "6xl": "4rem"
+};
+
+const spaces = {
+  sizes: {
+    ...chakraTheme.space,
+    full: "100%",
+    "3xs": "14rem",
+    "2xs": "16rem",
+    xs: "20rem",
+    sm: "24rem",
+    md: "28rem",
+    lg: "32rem",
+    xl: "36rem",
+    "2xl": "42rem",
+    "3xl": "48rem",
+    "4xl": "56rem",
+    "5xl": "64rem",
+    "6xl": "72rem"
+  }
+};
 
 const theme = {
   ...chakraTheme,
-  colors: {
-    ...chakraTheme.colors,
-    black: "#16161D"
-  },
+  breakpoints,
+  colors,
   fonts,
-  fontSizes: {
-    xs: "0.75rem",
-    sm: "0.875rem",
-    md: "1rem",
-    lg: "1.125rem",
-    xl: "1.25rem",
-    "2xl": "1.5rem",
-    "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
-    "6xl": "4rem"
-  },
-  breakpoints
+  fontSizes,
+  spaces
 };
 
 export default theme;
