a18b95d3e414007ef6f15bb4a3d362c586c95b0d
Eddie Naff <eddie@eddienaff.dev>
2020-10-17 19:09:04 -0700

    Add confirmation page

---
+++ src/pages/confirmation/[token].tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { NextPage } from "next";
+
+import { Wrapper } from "../../components/register.wrapper";
+import { useConfirmUserMutation } from "../../generated/graphql";
+import { Code, Stack, Text } from "@chakra-ui/core";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../../lib/utilities.create-urql-client";
+
+const Confirmation: NextPage<{ token: string }> = ({ token }) => {
+  const [, confirmUser] = useConfirmUserMutation();
+  const [confirmationError, setConfirmationError] = useState(null);
+  // const response = confirmUser({ token })
+  //   .then((data) => data)
+  //   .catch((err) => setConfirmationError(err.message))
+  //   .finally(() => console.log("confrim user fetch compolete"));
+
+  return (
+    <Wrapper>
+      <>
+        Confirmation page
+        <Text>{token}</Text>
+        <Stack isInline={false}>
+          <Code colorScheme="red">{confirmationError}</Code>
+          {/* <Code colorScheme="yellow">{response}</Code> */}
+        </Stack>
+        <Text>{}</Text>
+      </>
+    </Wrapper>
+  );
+};
+
+Confirmation.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string
+  };
+};
+
+export default withUrqlClient(createUrqlClient)(Confirmation);
