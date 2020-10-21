5773ad2adf5d68e9fdcb54e1d62e80045373ea2f
Eddie Naff <eddie@eddienaff.dev>
2020-10-21 11:12:58 -0700

    Add paginated versions of global posts query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,95 @@
-import { Box, Flex, Heading, Link, Text } from "@chakra-ui/core";
-import { withUrqlClient } from "next-urql";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Stack,
+  Text
+} from "@chakra-ui/core";
 import NextLink from "next/link";
 import React from "react";
-import { ImageBase } from "../components/image-base";
-import { useGetGlobalPostsQuery } from "../generated/graphql";
-import { createUrqlClient } from "../lib/utilities.create-urql-client";
+import { GlobalPostsStack } from "../components/home.global-posts";
+import { useGetGlobalPostsRelayQuery } from "../generated/graphql";
 
 function Index() {
-  const [{ data: dataGlobalPosts }] = useGetGlobalPostsQuery();
-  const posts = [];
-  if (dataGlobalPosts?.getGlobalPosts) {
-    for (const post of dataGlobalPosts.getGlobalPosts) {
-      posts.push(
-        <li key={post.id}>
-          <Flex
-            borderRadius=".2em"
-            boxShadow="2px 2px 2px 1px rgba(0, 0, 0, 0.2)"
-            flexDirection="column"
-            p={3}
-            my={["2em"]}
-            width={[1 / 2]}
-          >
-            <Heading>{post.title}</Heading>
+  const initialGlobalPostsVariables = {
+    after: null,
+    before: null,
+    first: 2,
+    last: null
+  };
+  // const [variables, setVariables] = useState<{
+  //   after?: null | string;
+  //   before?: null | string;
+  //   first: number | null;
+  //   last?: number | null;
+  // }>(initialGlobalPostsVariables);
 
-            <ImageBase images={post.images} />
-            <Text>{post.created_at}</Text>
-            <Text>
-              likes:{" "}
-              {post.likes && post.likes.length > 0 ? post.likes.length : "0"}
-            </Text>
+  const {
+    data: dataPosts,
+    error: errorPosts,
+    fetchMore: fetchMorePosts,
+    loading: loadingPosts
+  } = useGetGlobalPostsRelayQuery({
+    variables: initialGlobalPostsVariables
+  });
 
-            <Text>{post.text}</Text>
-          </Flex>
-        </li>
-      );
-    }
-  }
   return (
-    <Box>
-      <Text>Hello world</Text>
-      <NextLink href="/create-post" passHref>
-        <Link>create post</Link>
-      </NextLink>
-      <br />
-      <ul>{posts}</ul>
-    </Box>
+    <Flex flexDirection="column" alignItems="center">
+      <Flex alignItems="center" width={[1 / 2, 1 / 2, 1 / 2, "900px"]}>
+        <Heading>Branding</Heading>
+        <NextLink href="/create-post" passHref>
+          <Link ml="auto">create post</Link>
+        </NextLink>
+      </Flex>
+      <div>
+        <Stack mb={50}>
+          {dataPosts
+            ? dataPosts.getGlobalPostsRelay?.edges.map(
+                ({ node: { created_at, id, images, title, text } }) => {
+                  return (
+                    <Box key={id}>
+                      <Heading>{title}</Heading>
+                      <Box maxWidth="350px">
+                        <Image src={images && images[0] ? images[0].uri : ""} />
+                        <Text>{created_at}</Text>
+                        <Text>{text}</Text>
+                      </Box>
+                    </Box>
+                  );
+                }
+              )
+            : ""}
+          {dataPosts?.getGlobalPostsRelay?.pageInfo.hasNextPage === true ? (
+            <Button
+              colorScheme="teal"
+              type="button"
+              onClick={() => {
+                fetchMorePosts({
+                  variables: {
+                    first: initialGlobalPostsVariables.first,
+                    after:
+                      dataPosts?.getGlobalPostsRelay?.edges[
+                        dataPosts?.getGlobalPostsRelay?.edges.length - 1
+                      ].node.created_at
+                  }
+                });
+              }}
+            >
+              load more
+            </Button>
+          ) : null}
+        </Stack>
+      </div>
+      {/* <GlobalPostsStack
+        posts={dataPosts?.getGlobalPostsRelay?.edges}
+        postsError={errorPosts}
+        postsFetching={loadingPosts}
+      /> */}
+    </Flex>
   );
 }
 
-export default withUrqlClient(createUrqlClient, { ssr: false })(Index);
+export default Index;
