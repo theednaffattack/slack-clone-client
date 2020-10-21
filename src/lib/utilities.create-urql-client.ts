4ea78515c66529b322c5244034320a4120de1691
Eddie Naff <eddie@eddienaff.dev>
2020-10-21 11:15:09 -0700

    Add (failed) pagination example for Urql

--- src/lib/utilities.create-urql-client.ts
+++ src/lib/utilities.create-urql-client.ts
@@ -1,8 +1,12 @@
-import { cacheExchange } from "@urql/exchange-graphcache";
-import { dedupExchange, Exchange, fetchExchange } from "urql";
-import { pipe, tap } from "wonka";
+import { cacheExchange, DataField, Resolver } from "@urql/exchange-graphcache";
 import Router from "next/router";
-
+import {
+  dedupExchange,
+  Exchange,
+  fetchExchange,
+  stringifyVariables
+} from "urql";
+import { pipe, tap } from "wonka";
 import {
   ChangePasswordMutation,
   LoginMutation,
@@ -28,6 +32,100 @@ const errorExchange: Exchange = ({ forward }) => (ops$) => {
       }
     })
   );
+};
+
+// import { stringifyVariables } from "@urql/core";
+// import { Resolver, Variables, NullArray } from "../types";
+
+export interface PaginationParams {
+  cursorArgument?: string;
+  limitArgument?: string;
+}
+
+export const cursorPagination = ({
+  cursorArgument = "cursor"
+}: PaginationParams = {}): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+
+    const allFields = cache.inspectFields(entityKey);
+    console.log("ALL FIELDS", allFields);
+
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+
+    // Cache miss. No data was found for that fieldName.
+    if (size === 0) {
+      return undefined;
+    }
+
+    const results: DataField = [];
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+    console.log("VIEW FIELD KEY", fieldKey);
+    const isItInTheCache = cache.resolveFieldByKey(entityKey, fieldKey);
+    console.log("IS IT IN THE CACHE", isItInTheCache);
+    info.partial = !isItInTheCache;
+    // Check if data is in the cache. Return the data if true.
+
+    fieldInfos.forEach((fi) => {
+      const data = cache.resolveFieldByKey(entityKey, fi.fieldKey) as string[];
+
+      results.push(data);
+    });
+    console.log("VIEW CACHE RESULTS", results);
+    return results;
+    // const visited = new Set();
+    // let result: NullArray<string> = [];
+    // let prevOffset: number | null = null;
+
+    // for (let i = 0; i < size; i++) {
+    //   const { fieldKey, arguments: args } = fieldInfos[i];
+    //   if (args === null || !compareArgs(fieldArgs, args)) {
+    //     continue;
+    //   }
+
+    //   const links = cache.resolveFieldByKey(entityKey, fieldKey) as string[];
+    //   const currentOffset = args[cursorArgument];
+
+    //   if (
+    //     links === null ||
+    //     links.length === 0 ||
+    //     typeof currentOffset !== "number"
+    //   ) {
+    //     continue;
+    //   }
+
+    //   if (!prevOffset || currentOffset > prevOffset) {
+    //     for (let j = 0; j < links.length; j++) {
+    //       const link = links[j];
+    //       if (visited.has(link)) continue;
+    //       result.push(link);
+    //       visited.add(link);
+    //     }
+    //   } else {
+    //     const tempResult: NullArray<string> = [];
+    //     for (let j = 0; j < links.length; j++) {
+    //       const link = links[j];
+    //       if (visited.has(link)) continue;
+    //       tempResult.push(link);
+    //       visited.add(link);
+    //     }
+    //     result = [...tempResult, ...result];
+    //   }
+
+    //   prevOffset = currentOffset;
+    // }
+
+    // const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
+    // if (hasCurrentPage) {
+    //   return result;
+    // } else if (!(info as any).store.schema) {
+    //   return undefined;
+    // } else {
+    //   info.partial = true;
+    //   return result;
+    // }
+  };
 };
 
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
@@ -58,6 +156,11 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
     exchanges: [
       dedupExchange,
       cacheExchange({
+        resolvers: {
+          Query: {
+            getGlobalPostsSimplePagination: cursorPagination()
+          }
+        },
         updates: {
           Mutation: {
             changePassword: (_result, _args, cache, _info) => {
