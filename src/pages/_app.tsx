465f31126e3ebb088fa457cac7d6f554fd6f2d65
Eddie Naff <eddie@eddienaff.dev>
2020-10-22 09:46:26 -0700

    Add custom connection links to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,77 @@
 import { ChakraProvider, CSSReset } from "@chakra-ui/core";
 import { AppProps } from "next/app";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import { relayStylePagination } from "@apollo/client/utilities";
+import {
+  getMainDefinition,
+  relayStylePagination
+} from "@apollo/client/utilities";
+import { setContext } from "@apollo/link-context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import { onError } from "@apollo/client/link/error";
+import { RetryLink } from "@apollo/client/link/retry";
+import { HttpLink, split } from "@apollo/client";
+
+import theme from "../lib/theme";
+import { parseCookies } from "../lib/utilities.parse-cookies";
+import { isServer } from "../lib/utilities.is-server";
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_DEVELOPMENT_GQL_URI,
+  credentials: "include"
+});
+
+// Create a WebSocket link (browser only):
+const wsLink = !isServer()
+  ? new WebSocketLink(
+      new SubscriptionClient(
+        process.env.NEXT_PUBLIC_DEVELOPMENT_WEBSOCKET_URL!,
+        {
+          lazy: true,
+          reconnect: true
+        }
+      )
+    )
+  : null;
+
+const splitLink = !isServer()
+  ? split(
+      // split based on operation type
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+
+        return (
+          definition.kind === "OperationDefinition" &&
+          definition.operation === "subscription"
+        );
+      },
+      wsLink!,
+      httpLink
+    )
+  : httpLink;
+
+const authLink = setContext((_, { headers, req }) => {
+  const token = parseCookies(req)[process.env.NEXT_PUBLIC_COOKIE_PREFIX!];
+  console.log("CHECK HEADERS & TOKEN", { _, headers, token });
+
+  return {
+    headers: {
+      ...headers,
+      cookie: token ? `${process.env.NEXT_PUBLIC_COOKIE_PREFIX!}=${token}` : ""
+    }
+  };
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache({
@@ -13,11 +83,8 @@ const client = new ApolloClient({
       }
     }
   }),
-  credentials: "include",
-  uri: process.env.NEXT_PUBLIC_DEVELOPMENT_GQL_URI
+  link: errorLink.concat(authLink.concat(new RetryLink().concat(splitLink)))
 });
-
-import theme from "../lib/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
