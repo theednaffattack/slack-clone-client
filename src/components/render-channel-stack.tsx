c4edc4ff851832eb63d6463549ff98da325ebc55
Eddie Naff <eddie@eddienaff.dev>
2020-12-05 15:50:19 -0800

    Add individual channel renderer

--- src/components/render-channel-stack.tsx
+++ src/components/render-channel-stack.tsx
@@ -1,13 +1,106 @@
-import { VStack, StackDivider, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  StackDivider,
+  Text,
+  VStack
+} from "@chakra-ui/react";
+import React, { useEffect, useRef } from "react";
+import { useGetAllChannelThreadsQuery } from "../generated/graphql";
+import { deserialize_v2 } from "./rte.serialize";
 
-type RenderChannelStackProps = {};
+interface RenderChannelStackProps {
+  teamId: string;
+  channelId: string;
+}
 
-export const RenderChannelStack: React.FC<RenderChannelStackProps> = ({
-  children
-}) => {
+export function RenderChannelStack({
+  channelId,
+  teamId
+}: RenderChannelStackProps) {
+  const messageEl = useRef<HTMLDivElement>(null);
+  const messageBottomEl = useRef<HTMLDivElement>(null);
+
+  let body;
+
+  const { data, error, loading } = useGetAllChannelThreadsQuery({
+    skip: !channelId,
+    variables: {
+      channelId,
+      teamId
+    }
+  });
+
+  useEffect(() => {
+    messageEl.current?.scrollTo({
+      behavior: "auto",
+      top: messageBottomEl.current?.offsetTop
+    });
+  }, [data]);
+
+  if (!teamId || !channelId) {
+    body = (
+      <Center>
+        <Heading>Someting is missing!</Heading>
+        <Text>Team ID: {teamId}</Text>
+        <Text>Channel ID: {channelId}</Text>
+      </Center>
+    );
+  }
+  if (error) {
+    body = (
+      <Center>
+        <Heading>Error</Heading>
+        <Text>Team ID: {teamId}</Text>
+        <Text>Channel ID: {channelId}</Text>
+        <Text>{error.message}</Text>
+      </Center>
+    );
+  }
+  if (loading) {
+    body = [
+      { id: 0, message: "zero" },
+      { id: 1, message: "one" },
+      { id: 2, message: "two" }
+    ].map(({ id, message }, index) => (
+      <Box
+        key={`${id}-messages-list-${message}`}
+        as="li"
+        h="40px"
+        mt={index === 0 ? "auto" : 2}
+        listStyleType="none"
+      >
+        {message}
+      </Box>
+    ));
+  } else {
+    body = data?.getAllChannelThreads.map((thread, index) => {
+      return (
+        <Box
+          key={`${thread?.id}-messages-list`}
+          as="li"
+          // h="40px"
+          mx={5}
+          mt={index === 0 ? "auto" : 2}
+          listStyleType="none"
+        >
+          {thread.messages?.map((msg) => {
+            if (msg) {
+              return <Flex key={msg.id}>{deserialize_v2(msg.message)}</Flex>;
+            }
+            return;
+          })}
+        </Box>
+      );
+    });
+  }
+
   return (
     <VStack
       id="viewer"
+      ref={messageEl}
       gridColumn={3}
       gridRow={2}
       divider={<StackDivider key={Math.random()} borderColor="gray.200" />}
@@ -17,15 +110,9 @@ export const RenderChannelStack: React.FC<RenderChannelStackProps> = ({
       h="100%"
       overflow="auto"
     >
-      {children}
-      <Flex
-        w="100%"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        CHANNEL BROWSER
-      </Flex>
+      {body}
+
+      <Box id="dm-stack-bottom" ref={messageBottomEl}></Box>
     </VStack>
   );
-};
+}
