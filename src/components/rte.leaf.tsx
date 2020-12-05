2edac756efa841ee4c7f66cf555cdf12f9c7d6d5
Eddie Naff <eddie@eddienaff.dev>
2020-12-05 15:51:56 -0800

    Improve styling

--- src/components/rte.leaf.tsx
+++ src/components/rte.leaf.tsx
@@ -1,6 +1,6 @@
 export const Leaf = ({ attributes, children, leaf }: any) => {
   if (leaf.bold) {
-    children = <strong>{children}</strong>;
+    children = <strong {...attributes}>{children}</strong>;
   }
 
   if (leaf.code) {
@@ -16,17 +16,30 @@ export const Leaf = ({ attributes, children, leaf }: any) => {
           // width: "650px"
         }}
       >
-        <code>{children}</code>
+        <code
+          {...attributes}
+          style={{
+            backgroundColor: "#eee",
+            fontFamily:
+              "Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif",
+            maxHeight: "600px",
+            overflow: "auto",
+            padding: "5px"
+            // width: "650px"
+          }}
+        >
+          {children}
+        </code>
       </pre>
     );
   }
 
   if (leaf.italic) {
-    children = <em>{children}</em>;
+    children = <em {...attributes}>{children}</em>;
   }
 
   if (leaf.underline) {
-    children = <u>{children}</u>;
+    children = <u {...attributes}>{children}</u>;
   }
 
   return <span {...attributes}>{children}</span>;
