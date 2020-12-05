42b4972f6a32d6078097ec364145269d14ad692b
Eddie Naff <eddie@eddienaff.dev>
2020-12-05 15:54:05 -0800

    Add Rich Text previewer

---
+++ src/components/rte.preview.channel-threads.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useMemo } from "react";
+import { createEditor, Node } from "slate";
+import { Editable, Slate, withReact } from "slate-react";
+
+import { Element } from "./rte.element.channel-threads";
+import { Leaf } from "./rte.leaf";
+
+interface PreviewProps {
+  formValue: Node[];
+}
+
+export function Preview({ formValue }: PreviewProps) {
+  console.log("VIEW THEE INCOMING VALUES", formValue);
+
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const renderElement = useCallback((props: any) => <Element {...props} />, []);
+
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  return (
+    <Slate
+      editor={editor}
+      value={formValue}
+      onChange={(value) => console.log("VIEWER CHANGE", value)}
+    >
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        readOnly
+        placeholder="Enter some plain text..."
+      />
+    </Slate>
+  );
+}
