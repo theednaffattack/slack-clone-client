ad27ae35d49cb5d9a6bab718af2e04117c89aad4
Eddie Naff <eddie@eddienaff.dev>
2020-12-20 11:03:57 -0800

    Get signature from storage host before upload

--- src/components/channel.rte.tsx
+++ src/components/channel.rte.tsx
@@ -1,9 +1,8 @@
 import { Flex } from "@chakra-ui/react";
-import isHotkey from "is-hotkey";
-import produce from "immer";
-import "emoji-mart/css/emoji-mart.css";
 import { Picker } from "emoji-mart";
-import { useDropzone } from "react-dropzone";
+import "emoji-mart/css/emoji-mart.css";
+import produce from "immer";
+import isHotkey from "is-hotkey";
 import React, {
   useCallback,
   useEffect,
@@ -21,13 +20,21 @@ import {
   Slate,
   withReact
 } from "slate-react";
-
 import {
-  GetAllChannelThreadsQuery,
   GetAllChannelThreadsDocument,
+  GetAllChannelThreadsQuery,
   GetAllChannelThreadsQueryVariables,
-  useAddThreadToChannelMutation
+  S3SignatureAction,
+  useAddThreadToChannelMutation,
+  useSignS3FilesMutation
 } from "../generated/graphql";
+import { formatFilename, uploadToS3 } from "../lib/channel.rte.functions";
+import {
+  EmojiDataReplacement,
+  insertEmoji,
+  withEmoji
+} from "../lib/rte.plugin.with-emoji";
+import { insertMention, withMentions } from "../lib/rte.plugin.with-mentions";
 import { withLinks } from "../lib/rte.with-links";
 import { CHARACTERS } from "../lib/temp.character-names";
 import { Element } from "./rte.element";
@@ -35,15 +42,9 @@ import { Leaf } from "./rte.leaf";
 import { toggleMark } from "./rte.mark-button";
 import { serialize } from "./rte.serialize";
 import { Toolbar } from "./rte.toolbar";
-import { Portal } from "./slate-components";
-import { insertImage, withImages } from "./rte.with-images";
-import { withMentions, insertMention } from "../lib/rte.plugin.with-mentions";
-import {
-  EmojiDataReplacement,
-  insertEmoji,
-  withEmoji
-} from "../lib/rte.plugin.with-emoji";
+import { ImageProps, insertInlineImage, withImages } from "./rte.with-images";
 import { withUploadedImages } from "./rte.with-uploaded-images";
+import { Portal } from "./slate-components";
 
 interface HOTKEYINT {
   [key: string]: string;
@@ -68,7 +69,7 @@ interface ChannelRichTextInputProps {
   value: Node[];
 }
 
-export interface FilePreview extends File {
+export interface FilePreview extends FileList {
   preview: string;
 }
 
@@ -85,7 +86,7 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
   value: formValue
 }) => {
   const [addThreadMessage, { client }] = useAddThreadToChannelMutation();
-
+  const [signFile] = useSignS3FilesMutation();
   const mentionPanelRef = useRef<HTMLDivElement>(null);
   const [value, setValue] = useState<Node[]>(RESET);
   const [target, setTarget] = useState<Range | null>(null);
@@ -118,16 +119,17 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
   ).slice(0, 10);
 
   const onKeyDown = useCallback(
-    (event, formValue, invitees) => {
-      const { selection } = editor;
-      if (selection) {
-        const [start] = Range.edges(selection);
+    async (event, invitees) => {
+      // event.preventDefault();
+      // const { selection } = editor;
+      // if (selection) {
+      //   const [start] = Range.edges(selection);
 
-        Transforms.select(editor, {
-          anchor: start,
-          focus: start
-        });
-      }
+      //   Transforms.select(editor, {
+      //     anchor: start,
+      //     focus: start
+      //   });
+      // }
 
       // Insert line breaks for "Shift + Enter" keys
       if (isShiftEnterHotkey(event)) {
@@ -139,14 +141,63 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
       if (isEnterHotkey(event)) {
         event.preventDefault();
         if (!target) {
-          const strValues = value
-            .map((node) => {
-              return serialize(node);
+          const strValues = await Promise.all(
+            value.map(async (node) => {
+              if (node.type === "data-image") {
+                const preppedFile = {
+                  lastModified: (node as any).file.lastModified,
+                  lastModifiedDate: (node as any).file.lastModifiedDate,
+                  name: formatFilename((node as any).file.name),
+                  // path: (node as any).file.path,
+                  size: (node as any).file.size,
+                  type: (node as any).file.type,
+                  webkitRelativePath: (node as any).file.webkitRelativePath
+                };
+
+                let response;
+
+                try {
+                  response = await signFile({
+                    variables: {
+                      files: [preppedFile],
+                      action: S3SignatureAction.PutObject
+                    }
+                  });
+                } catch (error) {
+                  console.warn("SIGN FILE ERROR", error);
+                }
+
+                const newNode = {
+                  ...node,
+                  url: response?.data?.signS3Files.signatures[0].uri
+                };
+                // PUT ADDITIONAL UPLOAD VIA AXIOS TO STORAGE BUCKET
+
+                try {
+                  await uploadToS3(
+                    node.file as File,
+                    response?.data?.signS3Files.signatures[0].signedRequest ??
+                      ""
+                  );
+                } catch (error) {
+                  console.warn("UPLOAD ERROR", error);
+                }
+
+                return serialize(newNode);
+              } else {
+                return serialize(node);
+              }
             })
-            .join("");
+          );
+
+          console.log("STRING VLAUES", strValues.join(""));
+
           addThreadMessage({
             update: (cache, data) => {
-              let threadsInCache;
+              let threadsInCache:
+                | GetAllChannelThreadsQuery
+                | null
+                | undefined = undefined;
               try {
                 threadsInCache = cache.readQuery<
                   GetAllChannelThreadsQuery,
@@ -216,15 +267,15 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
                 teamId,
                 created_at: "",
                 invitees: [...invitees],
-                message: strValues
+                message: strValues.join("")
               }
             }
           });
 
           // THIS IS WHERE WE CLEAR THE INPUT
-          // Move selection to the beginning before...
-          // clearing the input. It's too fast
-          // somehow so the editor will begin point...
+          // Move selection to the beginning of the
+          // input field before clearing the input.
+          // It's too fast somehow so the editor will point
           // at a location which no longer exists.
           Transforms.select(editor, {
             anchor: { path: [0, 0], offset: 0 },
@@ -285,6 +336,8 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
             break;
         }
       }
+
+      console.log("KEY DOWN");
     },
     [index, search, target, value]
   );
@@ -311,36 +364,29 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
   }, [chars.length, editor, index, search, target]);
 
   // BEGIN DROPZONE SECTION
-  const [files, setFiles] = React.useState<FilePreview[]>([]);
-  const { getInputProps, getRootProps, open } = useDropzone({
-    noDragEventsBubbling: true,
+  const [files, setFiles] = React.useState<
+    { type: "data-image" | "image"; url: string }[] | null
+  >(null);
 
-    onDrop: (acceptedFiles: File[]) => {
-      // Place images in local state for later
-      // submission.
-      setFiles(
-        acceptedFiles.map((file) =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file)
-          })
-        )
-      );
-    }
-  });
-
-  React.useEffect(() => {
-    files.forEach((file) => {
-      insertImage({
-        editor,
-        type: "data-image",
-        url: URL.createObjectURL(file)
-      });
-    });
-    return () => {
-      // Make sure to revoke the data uris to avoid memory leaks
-      files.forEach((file) => URL.revokeObjectURL(file.preview));
-    };
-  }, [files]);
+  // React.useEffect(() => {
+  //   if (files) {
+  //     insertMultipleImages({
+  //       editor,
+  //       imageNodes: files
+  //     });
+  //   }
+  //   // files.forEach((file) => {
+  //   //   insertImage({
+  //   //     editor,
+  //   //     type: "data-image",
+  //   //     url: URL.createObjectURL(file)
+  //   //   });
+  //   // });
+  //   return () => {
+  //     // Make sure to revoke the data uris to avoid memory leaks
+  //     files?.forEach((file) => URL.revokeObjectURL(file.url));
+  //   };
+  // }, [files]);
 
   // END DROPZONE SECTION
 
@@ -361,6 +407,14 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
   }, [editor]);
 
   const divRef = React.useRef<HTMLDivElement>(null);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const openFileDialog = () => {
+    if (inputRef && inputRef.current) {
+      console.log("VIEW INPUT ELEMENT VIA REF", inputRef.current);
+
+      inputRef.current?.click();
+    }
+  };
   const focusEditor = React.useCallback(
     (e: React.MouseEvent) => {
       if (e.target === divRef.current) {
@@ -378,7 +432,6 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
       mx={3}
       borderRadius="4px"
       overflow="hidden"
-      {...getRootProps}
     >
       <Slate
         editor={editor as ReactEditor}
@@ -429,7 +482,9 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
       >
         <Editable
           renderElement={renderElement}
-          onKeyDown={(event) => onKeyDown(event, formValue, invitees)}
+          onKeyDown={(event) => {
+            onKeyDown(event, invitees);
+          }}
           renderLeaf={renderLeaf}
           placeholder={channelName ? `Message #${channelName}` : "enter text"}
           spellCheck
@@ -440,7 +495,71 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
             padding: "8px"
           }}
         />
-        <input {...getInputProps()} style={{ display: "none" }} />
+        <input
+          type="file"
+          name="file"
+          multiple
+          ref={inputRef}
+          onChange={(event) => {
+            const { files: uploadFiles } = event.target;
+            const cache: ImageProps[] = [];
+            // setFiles(
+            //   Array.from(uploadFiles).map((file) => {
+            //     return {
+            //       url: URL.createObjectURL(file),
+            //       type: "data-image"
+            //     };
+            //   })
+            // );
+            if (uploadFiles && uploadFiles.length > 0) {
+              for (const file of Array.from(uploadFiles)) {
+                const reader = new FileReader();
+                const [mime] = file.type.split("/");
+
+                if (mime === "image") {
+                  reader.addEventListener("load", () => {
+                    const url = reader.result;
+                    console.log("VIEW URL INFO", { url, type: typeof url });
+                    // push into cache
+                    cache.push({
+                      type: "data-image",
+                      url: url as string
+                    });
+
+                    console.log("PUSHING", {
+                      type: "data-image",
+                      url: url as string,
+                      cacheLength: cache.length
+                    });
+                    insertInlineImage({
+                      editor,
+                      type: "data-image-list",
+                      url: url as string
+                    });
+                    // insertImage({
+                    //   editor,
+                    //   type: "data-image",
+                    //   url: url as string
+                    // });
+                  });
+
+                  reader.readAsDataURL(file);
+                }
+              }
+            }
+
+            console.log("VIEW CACHE", cache);
+
+            // if (cache.length === 1) {
+            //   insertImage({ editor, type: cache[0].type, url: cache[0].url });
+            // }
+            // if (cache.length > 1) {
+            //   insertMultipleImages({ editor, imageNodes: cache });
+            // }
+          }}
+          style={{ display: "none" }}
+        />
+
         {target && (
           <Portal>
             <div
@@ -513,8 +632,9 @@ export const ChannelRichTextInput: React.FC<ChannelRichTextInputProps> = ({
         <Toolbar
           channelId={channelId}
           editor={editor}
+          inputRef={inputRef}
           invitees={invitees}
-          openFileDialog={open}
+          openFileDialog={openFileDialog}
           setTarget={setTarget as any}
           setIndex={setIndex}
           setSearch={setSearch}
