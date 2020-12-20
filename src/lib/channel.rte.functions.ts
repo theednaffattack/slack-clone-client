d069aae2c9bbf9c996d6e75ee276a23440eb3f37
Eddie Naff <eddie@eddienaff.dev>
2020-12-20 11:15:09 -0800

    Add module to add files to storage

---
+++ src/lib/channel.rte.functions.ts
@@ -0,0 +1,49 @@
+import { format } from "date-fns";
+import axios from "axios";
+
+format(new Date(2014, 1, 11), "MM/dd/yyyy");
+//=> '02/11/2014'
+
+export async function uploadToS3(file: File, signedRequest: string) {
+  const options = {
+    headers: {
+      "Content-Type": file.type
+    }
+  };
+  console.log("VIEW UPLOAD TO S3 OPTIONS", { file, options });
+
+  await axios.put(signedRequest, file, options);
+}
+
+export function formatFilename(filename: string): string {
+  // from: https://stackoverflow.com/questions/48495289/javascript-not-able-to-rename-file-before-upload
+  const date = format(new Date(), "yyyyMMdd");
+  const randomString = Math.random().toString(36).substring(2, 7);
+  const fileExt = filename.split(".")[filename.split(".").length - 1];
+  const cleanedFileameWithoutExt = filename
+    .replace(`.${fileExt}`, "")
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "-");
+  const newFileName = `${date}-${randomString}-${cleanedFileameWithoutExt}.${fileExt}`;
+  return newFileName;
+}
+
+function formatFilename_Original(filename: string): string {
+  const date = format(new Date(), "yyyyMMdd");
+  const randomString = Math.random().toString(36).substring(2, 7);
+  const cleanFileName = filename.toLowerCase().replace(/[^a-z0-9]/g, "-");
+  const newFilename = `images/${date}-${randomString}-${cleanFileName}`;
+  return newFilename.substring(0, 60);
+}
+
+export async function submit(file: File) {
+  const response = await s3SignFile({
+    variables: {
+      filename: formatFilename(file.name),
+      filetype: file.type
+    }
+  });
+
+  const { signedRequest, uri } = response.data.signS3File;
+  await uploadToS3(file, signedRequest);
+}
