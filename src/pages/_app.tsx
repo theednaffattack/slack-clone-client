a289fa16fdf3d50289e76fc2dd9092d2d8f4a59a
Eddie Naff <eddie@eddienaff.dev>
2021-03-07 17:37:54 -0800

    Experimen with auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,18 @@
-import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import Router from "next/router";
-import { useEffect } from "react";
-import { useApollo } from "../lib/config.apollo-client";
+import React, { useEffect } from "react";
+import AuthProvider from "../components/auth-provider";
 import theme from "../lib/theme";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  const apolloClient = useApollo(pageProps); // pageProps.initialApolloState
-
   const syncLogout = (event: StorageEvent) => {
     if (event.key === "logout") {
-      console.log("logged out from storage!");
       Router.push("/login");
     }
   };
 
+  // multi-window/tab logout
   useEffect(() => {
     window.addEventListener("storage", syncLogout);
 
@@ -27,12 +24,13 @@ function MyApp({ Component, pageProps, router }: AppProps) {
 
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <ApolloProvider client={apolloClient}>
-        <CSSReset />
-        <Component {...pageProps} router={router} />
-      </ApolloProvider>
+      <CSSReset />
+      <AuthProvider>
+        <Component syncLogout={syncLogout} {...pageProps} router={router} />
+      </AuthProvider>
     </ChakraProvider>
   );
 }
+// }
 
 export default MyApp;
