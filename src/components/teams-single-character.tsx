32c1df351498e16754a3d8d43961921b7e471341
Eddie Naff <eddie@eddienaff.dev>
2021-03-09 15:38:58 -0800

    Remove menu from single char teams button

--- src/components/teams-single-character.tsx
+++ src/components/teams-single-character.tsx
@@ -1,80 +1,35 @@
-import { ChevronDownIcon } from "@chakra-ui/icons";
-import {
-  Button,
-  Flex,
-  Heading,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Portal
-} from "@chakra-ui/react";
+import { Button, Heading } from "@chakra-ui/react";
 import { Router } from "next/router";
 import React from "react";
 
 export function TeamMenuSingleCharacter({
   id,
   name,
-  router
+  router,
+  selected
 }: {
   id: string;
   name: string;
   router: Router;
+  selected: boolean;
 }) {
-  function handleInviteTeamMemberClick(
-    event: React.MouseEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    event.preventDefault();
-    // router.push(`/view-team/${id}?viewing=teams_browser&action=find_channel`);
-
-    router.push({
-      href: "/view-team/",
-      query: { viewing: "teams_invite_member", id }
-    });
-  }
   return (
-    <Menu>
-      <MenuButton
-        as={Button}
-        // colorScheme="pink"
-        bg="transparent"
-        borderWidth="1px"
-        borderColor="transparent"
-        rightIcon={<ChevronDownIcon />}
-        size="lg"
-        transition="all 0.2s"
-        borderRadius="md"
-        _hover={{ borderColor: "gray.400" }}
-        _expanded={{ bg: "transparent" }}
-        _focus={{ boxShadow: "outline" }}
-      >
-        <Heading size="2xl">{name.charAt(0)}</Heading>
-      </MenuButton>
-      <Portal>
-        <MenuList id="team-menu">
-          <Flex>
-            <Heading>{name}</Heading>
-          </Flex>
-          <MenuItem
-            isTruncated
-            onClick={(evt) => {
-              handleInviteTeamMemberClick(evt, id);
-            }}
-          >
-            Invite people to {name}
-          </MenuItem>
-          <MenuDivider />
-          <MenuItem>Preferences</MenuItem>
-          <MenuDivider />
-          <MenuItem>Customize</MenuItem>
-          <MenuDivider />
-          <MenuItem>Tools</MenuItem>
-          <MenuDivider />
-          <MenuItem>Sign out</MenuItem>
-        </MenuList>
-      </Portal>
-    </Menu>
+    <Button
+      _hover={{ borderColor: "gray.400" }}
+      _expanded={{ bg: "transparent" }}
+      _focus={{ boxShadow: "outline" }}
+      bg="transparent"
+      borderColor={selected ? "limegreen" : "transparent"}
+      borderRadius="md"
+      borderWidth="1px"
+      onClick={(evt) => {
+        evt.preventDefault();
+        router.push(`/view-team/${id}`);
+      }}
+      size="lg"
+      transition="all 0.2s"
+    >
+      <Heading size="2xl">{name.charAt(0)}</Heading>
+    </Button>
   );
 }
