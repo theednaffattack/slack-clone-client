0a192218c418877521b81cc0d5b0bb503639e1fb
Eddie Naff <eddie@eddienaff.dev>
2021-03-26 15:30:29 -0700

    Add channel browser to the channel add page

--- src/components/render-channel-browser.tsx
+++ src/components/render-channel-browser.tsx
@@ -1,11 +1,63 @@
-import { VStack, StackDivider, Flex } from "@chakra-ui/react";
+import {
+  Avatar,
+  AvatarGroup,
+  Flex,
+  StackDivider,
+  Text,
+  VStack
+} from "@chakra-ui/react";
+import React from "react";
+import { useLoadChannelsByTeamIdQuery } from "../generated/graphql";
 
-export const RenderChannelBrowser: React.FC = ({ children }) => {
+interface RenderChannelBrowserProps {
+  teamId: string;
+}
+
+export const RenderChannelBrowser: React.FC<RenderChannelBrowserProps> = ({
+  children,
+  teamId
+}) => {
+  const {
+    data: dataChannels
+    // error: errorChannels
+  } = useLoadChannelsByTeamIdQuery({
+    skip: teamId === null,
+    variables: { teamId: teamId ? teamId : "" }
+  });
+
+  const formattedChannels = [];
+  if (dataChannels) {
+    for (const channel of dataChannels?.loadChannelsByTeamId) {
+      // do stuff
+      const formattedInvitees = [];
+      if (channel.invitees) {
+        for (const person of channel.invitees) {
+          formattedInvitees.push(
+            <Avatar
+              key={`channel-avatar-${person?.id}`}
+              name={`${person?.username}`}
+              src={person?.profileImageUri ?? undefined}
+            />
+          );
+        }
+      }
+      formattedChannels.push(
+        <Flex key={channel.id} flexDirection="column">
+          <Flex>{channel.name}</Flex>
+          <Flex>Invitees: {channel.invitees?.length.toString()}</Flex>
+
+          <AvatarGroup size="md" max={2}>
+            {formattedInvitees}
+          </AvatarGroup>
+        </Flex>
+      );
+    }
+  }
   return (
     <VStack
       id="viewer"
-      gridColumn={3}
-      gridRow={2}
+      // gridColumn={3}
+      // gridRow={2}
       divider={<StackDivider key={Math.random()} borderColor="gray.200" />}
       spacing={4}
       align="stretch"
@@ -14,14 +66,18 @@ export const RenderChannelBrowser: React.FC = ({ children }) => {
       overflow="auto"
     >
       {children}
-      <Flex
-        w="100%"
-        flexDirection="column"
+      <VStack
         alignItems="center"
-        justifyContent="center"
+        divider={<StackDivider key={Math.random()} borderColor="gray.200" />}
+        flexDirection="column"
+        w="100%"
+        h="100%"
       >
-        CHANNEL BROWSER
-      </Flex>
+        <Flex>
+          <Text>CHANNEL BROWSER</Text>
+        </Flex>
+        {formattedChannels}
+      </VStack>
     </VStack>
   );
 };
